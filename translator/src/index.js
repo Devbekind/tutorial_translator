1c1fd83324f003eb2cbddb3e60c7ae7474dddfe8
Devbeekind <mykyta.kolysnychenko@nure.ua>
2022-07-19 08:12:41 +0200

    0.1
    working translator

---
+++ translator/src/index.js
@@ -0,0 +1,20 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'
+ 
+import './i18n';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactDOM.render (
+  <React.StrictMode>
+    <Suspense fallback={<div>Loading...</div>}>
+        <App />
+    </Suspense>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+ 
+
