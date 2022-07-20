e06269485d8b60cdb87c6aafd1825f9837da0cc8
Devbeekind <mykyta.kolysnychenko@nure.ua>
2022-07-20 15:23:27 +0200

    0.2

--- translator/src/App.js
+++ translator/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { useTranslation } from "react-i18next";
-
+import Demo from './demo';
 function App() {
   const { t, i18n } = useTranslation();
 
@@ -11,18 +11,58 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => changeLanguage("en")}><img id="photo" src="./img/eng.png"/></button>
-      <button onClick={() => changeLanguage("ru")}>RU</button>
-      <button onClick={() => changeLanguage("ua")}>UA</button>
-      <hr />
-      <div><h1>{t("title")}</h1></div>
-      <div>{t("description.part1")}</div>
-      
-      <div>{t("description.part2")}</div>
-      <div>{t("description.part3")}</div>
+     <div className="Header_novis">  
+      <table> 
        
+         
+      
+        <tr>
+        <td> 
+        <div className="logo"> 
+        <img src=""/>
+        
+        </div>
+        </td><td><div className="title"><h1>{t("title")}</h1></div></td>
+        <td> 
+        <div className="buttons">
+      <button className="buttonen" onClick={() => changeLanguage("en")}><img id="photo" src="./img/eng.png"/></button>
+      <button className="buttondk" onClick={() => changeLanguage("da-DK")}><img id="photo" src="./img/dk.png"/></button>
+      <button className="buttonua" onClick={() => changeLanguage("ua")}><img id="photo" src="./img/ua.jpg"/></button>
+      </div>
+      </td>
+      </tr>
+      </table>
+      </div>  
+      <div className='demo'>
+        <table>
+            <tc>
+              <td>
+          <Demo> <div className="content">content</div>content</Demo>
+              </td>
+            </tc>
+            <tc>
+              <td>
+               <div   >  
+               <div className="descriptionpart1">{t("description.part1")}</div>
+               </div>
+              </td>
+            </tc>
+        
+         </table>
+          </div>
+       
+      <div className="textmenu"> 
+       
+       
+      
+       
+      {/* <div className="descriptionpart3">{t("description.part3")}</div> */}
+       
+      </div>
 
-
+      <div className="footer">
+      <div className="descriptionpart2">{t("description.part2")}</div>
+      </div>
     </div>
   );
 }
--- translator/src/index.js
+++ translator/src/index.js
@@ -2,19 +2,24 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
- 
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
 import './i18n';
-
+ 
+ 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+  
 ReactDOM.render (
   <React.StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
+     
         <App />
+         
+     
     </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
  
-
